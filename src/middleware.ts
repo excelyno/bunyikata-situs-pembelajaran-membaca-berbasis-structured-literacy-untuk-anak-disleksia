ff50a101c296a8f66b292a23a91230d030d2a3f3
excelyno <excellentqweee@gmail.com>
2026-05-01 15:12:10 +0700

    push

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,23 +5,24 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get('bunyikata_token')?.value;
   const path = req.nextUrl.pathname;
 
-  // Cek apakah halaman yang dibuka adalah halaman terproteksi
-  const isProtectedRoute = path.startsWith('/dashboard') || path.startsWith('/games');
+  const isProtectedRoute = path.startsWith('/dashboard');
+  const isAuthRoute = path.startsWith('/login') || path.startsWith('/register');
 
-  // Jika belum login & buka halaman terproteksi -> Tendang ke /login
   if (!token && isProtectedRoute) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  // Jika SUDAH login & buka halaman /login -> Tendang ke dashboard (sementara arahkan ke root)
-  if (token && path === '/login') {
+  if (token && isAuthRoute) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
   return NextResponse.next();
 }
 
-// Tentukan rute mana saja yang dijaga Middleware
 export const config = {
-  matcher: ['/dashboard/:path*', '/games/:path*', '/login'],
+  matcher: [
+    '/dashboard/:path*', 
+    '/login', 
+    '/register/:path*'
+  ],
 };
